c25456a1f60e845a11a00d80a0fd620d33c0f7a8
vararais <varadigmaisnann@gmail.com>
2024-05-07 18:30:31 +0700

    Menambah Void

--- PointerTiga/PointerTiga.cpp
+++ PointerTiga/PointerTiga.cpp
@@ -18,6 +18,20 @@ public:
     void setId(int pId);
     void displayAll(orang& a);
 };
+
+void siswa::displayAll(orang& a)
+{
+    cout << id << endl << a.nama;
+}
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+
 int main()
 {
     
