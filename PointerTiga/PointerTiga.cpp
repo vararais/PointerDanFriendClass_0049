fe844a9af00ec8205ae6095eef8fdec9fd30d674
vararais <varadigmaisnann@gmail.com>
2024-05-07 18:21:46 +0700

    Menambah Class Siswa

--- PointerTiga/PointerTiga.cpp
+++ PointerTiga/PointerTiga.cpp
@@ -10,6 +10,14 @@ public:
     void setNama(string pNama);
     friend class siswa;
 };
+
+class siswa {
+private:
+    int id;
+public:
+    void setId(int pId);
+    void displayAll(orang& a);
+};
 int main()
 {
     
